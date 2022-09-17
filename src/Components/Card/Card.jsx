e123067ed0c445bf9194f293401742b4bfb25cab
Santi <xxsmating@gmail.com>
2022-09-16 21:38:49 -0300

    eliminaicon de keys poco útiles

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -3,13 +3,13 @@ import './card.css';
 const Card = ({ item }) => {
     const { id, name, price, imageS, stock} = item;
     return (
-        <div className='col-lg-4 col-md-6 col-sm-12' key={name}>
-            <img src={imageS.replaceAll('./','../')} alt={name} key={imageS} className="ms-auto me-auto card-img bg-dark" />
-            <h2 className="text-white" key={name}>{name}</h2>
-            <p className='text-danger' key={price}>${price}</p>
-            <Link to={`/detail/${id}`} key={id} data-id={id} className="detalle">Ir a detalle de producto
+        <div className='col-lg-4 col-md-6 col-sm-12'>
+            <img src={imageS.replaceAll('./','../')} alt={name} className="ms-auto me-auto card-img bg-dark" />
+            <h2 className="text-white">{name}</h2>
+            <p className='text-danger'>${price}</p>
+            <Link to={`/detail/${id}`} data-id={id} className="detalle">Ir a detalle de producto
             </Link>
-            <p key={stock} className="text-light mt-2">Stock: {stock}</p>
+            <p className="text-light mt-2">Stock: {stock}</p>
         </div>
     )
 }
--- src/Components/CardDetail/CardDetail.jsx
+++ src/Components/CardDetail/CardDetail.jsx
@@ -10,13 +10,13 @@ const CardDetail = ({ item }) => {
         isInCart(id);
     }
     return (
-        <div className='col-lg-12 col-md-12 col-sm-12 productDetail' key={id}>
+        <div className='col-lg-12 col-md-12 col-sm-12 productDetail'>
             <div className='col-lg-6 col-md-12 col-sm-12 img'>
-                <img src={imageS.replaceAll('./', '../')} alt={nameS} key={imageS} data-id={imageS} className="ms-auto me-auto card-img bg-dark" />
+                <img src={imageS.replaceAll('./', '../')} alt={nameS} data-id={imageS} className="ms-auto me-auto card-img bg-dark" />
             </div>
             <div className='col-lg-6 col-md-12 col-sm-12'>
                 <h4 className='text-danger'>{nameS}</h4>
-                <p className='text-danger' key={price}>${price}</p>
+                <p className='text-danger'>${price}</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos obcaecati unde qui illum adipisci, molestiae corporis illo nobis nisi, veritatis dolor eius odit laudantium ipsum tempora maxime aliquid tenetur sit distinctio praesentium maiores repellat. Reiciendis hic soluta possimus eveniet quia corporis mollitia, ratione accusamus minus non atque veritatis odit similique.</p> 
                 <ButtonNumber stock={stock} initial={initial} onAdd={onAdd} id={id} nameS={nameS} imageS={imageS} price={price}/> 
             </div>
