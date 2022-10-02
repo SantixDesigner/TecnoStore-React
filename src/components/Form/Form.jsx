2de97e969c52ce2e843ef37b7be91d927b475707
Santi <xxsmating@gmail.com>
2022-10-01 21:01:48 -0300

    ..

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,38 +1,38 @@
 
 import './form.css'
-import { getFirestore, addDoc, collection } from 'firebase/firestore'
+import { addDoc, collection } from 'firebase/firestore'
 import { useCartContext } from "../../context/CartContext";
 import Swal from "sweetalert2";
+import { db } from '../../firebase/firebase'
 import { useState } from "react";
 import FormChildren from '../FormChildren/FormChildren';
 const Form = () => {
   const { products, getTotalPrice, clearProducts, getTotalQuantity } = useCartContext();
   const [form, setForm] = useState({
-      name: '',
-      email: '',
-      message: ''
+    name: '',
+    email: '',
+    message: ''
   });
   const [isSetOrderConfirmed, setOrderConfirmed] = useState(false);
   const changeHandler = (event) => {
-      const newForm = { ...form, [event.target.name]: event.target.value };
-      setForm(newForm);
+    const newForm = { ...form, [event.target.name]: event.target.value };
+    setForm(newForm);
   };
   const submitHandler = (event) => {
-      const items = products.map(item => { return { id: item.id, title: item.nameS, price: item.price, quantity: item.quantity } })
-      const order = { form, items: items, total: getTotalPrice() }
-      event.preventDefault();
-      const db = getFirestore();
-      const contactFormCollection = collection(db, "contactoform");
-      addDoc(contactFormCollection, order).then((snapshot) => {
-          Swal.fire({
-              title: 'Compra Finalizada',
-              text: `Tu codigo de seguimiento es ${snapshot.id}`,
-              icon: 'success',
-              footer: `Haz comprado ${getTotalQuantity()} productos a un precio total de $${getTotalPrice()}`,
-          })
-          setOrderConfirmed(true);
-      });
-      clearProducts();
+    const items = products.map(item => { return { id: item.id, title: item.nameS, price: item.price, quantity: item.quantity } })
+    const order = { form, items: items, total: getTotalPrice() }
+    event.preventDefault();
+    const contactFormCollection = collection(db, "contactoform");
+    addDoc(contactFormCollection, order).then((snapshot) => {
+      Swal.fire({
+        title: 'Compra Finalizada',
+        text: `Tu codigo de seguimiento es ${snapshot.id}`,
+        icon: 'success',
+        footer: `Haz comprado ${getTotalQuantity()} productos a un precio total de $${getTotalPrice()}`,
+      })
+      setOrderConfirmed(true);
+    });
+    clearProducts();
   }
   return (
     <div className='checkout-container'>
